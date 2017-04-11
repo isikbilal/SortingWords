432062214de45e2c9f98db1ed7bf3e24b02175db
Bilal Işık <bilal_isik@outlook.com>
2017-04-11 22:58:58 +0300

    Add files via upload

---
+++ Kelime Siralama/Siralama/Siralama/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Siralama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblsay = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.OpenFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(254, 225);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(272, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(272, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Metin Belgesinden(.txt) Oku";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(272, 148);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Metin Belgesine(.txt) Kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(272, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(177, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Tamamını Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(272, 206);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(177, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Seçili Olanı Sil";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Toplam Kayıt :";
+            // 
+            // lblsay
+            // 
+            this.lblsay.AutoSize = true;
+            this.lblsay.Location = new System.Drawing.Point(353, 248);
+            this.lblsay.Name = "lblsay";
+            this.lblsay.Size = new System.Drawing.Size(13, 13);
+            this.lblsay.TabIndex = 8;
+            this.lblsay.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(280, 55);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(141, 41);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "SIRALA";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 368);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.lblsay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Alfa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblsay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SaveFileDialog OpenFileDialog;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Kelime Siralama/Siralama/Siralama/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Siralama
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] kelimeler = textBox1.Text.Split(' ');
+            listBox1.Items.AddRange(kelimeler);
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            StreamReader oku;
+     
+            OpenFileDialog file = new OpenFileDialog();
+
+            file.Filter = "Text Dosyası |*.txt";
+            file.Title=("Txt Dosyası Seçme Ekranı");
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+
+                oku = File.OpenText(file.FileName);
+                string yazi;
+
+                while ((yazi = oku.ReadLine()) != null)
+                {
+                    listBox1.Items.Add(yazi.ToString());
+                }
+                oku.Close();
+            }
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.Filter = "Text Dosyası |*.txt";
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+               
+                StreamWriter yaz = new StreamWriter(savefile.FileName);
+
+                for(int i=0;i<listBox1.Items.Count;i++)
+                {
+                    yaz.WriteLine(listBox1.Items[i].ToString());
+                }
+
+                yaz.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblsay.Text = listBox1.Items.Count.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int itemsayi = listBox1.Items.Count;
+            int sayac = 0;
+            string[] kelimeler = { };
+            string[] kelimeler2 = new string[1000];
+            for (int i = 0; i < itemsayi; i++)
+            {
+                kelimeler = listBox1.Items[i].ToString().Split(' ');
+                foreach (string kelime in kelimeler)
+                {
+
+                    kelimeler2[sayac] = kelime;
+                    sayac++;
+                }
+
+            }
+            listBox1.Items.Clear();
+            for (int i = 0; i < kelimeler2.Length; i++)
+            {
+                for (int j = i + 1; j < kelimeler2.Length; j++)
+                {
+
+                    int sonuc = String.Compare(kelimeler2[i], kelimeler2[j]);
+                    if (sonuc < 0) { }
+
+                    else if (sonuc > 0)
+                    {
+                        string tut = kelimeler2[i];
+                        kelimeler2[i] = kelimeler2[j];
+                        kelimeler2[j] = tut;
+                    }
+
+
+                }
+
+            }
+
+            foreach (string kelime in kelimeler2)
+            {
+                if (kelime == null) { }
+                else
+                {
+                    listBox1.Items.Add(kelime);
+                }
+            }
+        }
+    }
+}
